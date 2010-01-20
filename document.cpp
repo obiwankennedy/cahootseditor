e5727b4e0979b6829e653fdfa9c8877e0916d4ae
chris.dimpfl <chris.dimpfl@ecc9928c-ffe0-11de-95d2-51bf8322dccd>
2010-01-20 01:20:41 +0000

    Changed the document layout to include a chat window.

--- document.cpp
+++ document.cpp
@@ -10,10 +10,10 @@ Document::Document(QWidget *parent) :
 {
     m_ui->setupUi(this);
 
-    cppHighlighter = new CppHighlighter(m_ui->textEdit->document());
+    cppHighlighter = new CppHighlighter(m_ui->codeTextEdit->document());
 
-    connect(m_ui->textEdit, SIGNAL(undoAvailable(bool)), this, SIGNAL(undoAvailable(bool)));
-    connect(m_ui->textEdit, SIGNAL(redoAvailable(bool)), this, SIGNAL(redoAvailable(bool)));
+    connect(m_ui->codeTextEdit, SIGNAL(undoAvailable(bool)), this, SIGNAL(undoAvailable(bool)));
+    connect(m_ui->codeTextEdit, SIGNAL(redoAvailable(bool)), this, SIGNAL(redoAvailable(bool)));
 
 //    m_ui->listWidget->hide();
 
@@ -30,62 +30,62 @@ Document::~Document()
 
 void Document::undo()
 {
-    m_ui->textEdit->undo();
+    m_ui->codeTextEdit->undo();
 }
 
 void Document::redo()
 {
-    m_ui->textEdit->redo();
+    m_ui->codeTextEdit->redo();
 }
 
 void Document::cut()
 {
-    m_ui->textEdit->cut();
+    m_ui->codeTextEdit->cut();
 }
 
 void Document::copy()
 {
-    m_ui->textEdit->copy();
+    m_ui->codeTextEdit->copy();
 }
 
 void Document::paste()
 {
-    m_ui->textEdit->paste();
+    m_ui->codeTextEdit->paste();
 }
 
 bool Document::isUndoable()
 {
-    return m_ui->textEdit->document()->isUndoAvailable();
+    return m_ui->codeTextEdit->document()->isUndoAvailable();
 }
 
 bool Document::isRedoable()
 {
-    return m_ui->textEdit->document()->isRedoAvailable();
+    return m_ui->codeTextEdit->document()->isRedoAvailable();
 }
 
 bool Document::isModified()
 {
-    return m_ui->textEdit->document()->isModified();
+    return m_ui->codeTextEdit->document()->isModified();
 }
 
 QString Document::getPlainText()
 {
-    return m_ui->textEdit->toPlainText();
+    return m_ui->codeTextEdit->toPlainText();
 }
 
 void Document::setPlainText(QString text)
 {
-    m_ui->textEdit->setPlainText(text);
+    m_ui->codeTextEdit->setPlainText(text);
 }
 
 void Document::setModified(bool b)
 {
-    m_ui->textEdit->document()->setModified(b);
+    m_ui->codeTextEdit->document()->setModified(b);
 }
 
 bool Document::eventFilter(QObject *object, QEvent *event)
 {
-    if (event->type() == QEvent::KeyPress && object == m_ui->textEdit) {
+    if (event->type() == QEvent::KeyPress && object == m_ui->codeTextEdit) {
         QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
         if (!keyEvent->text().isEmpty()) {
             socket->write(keyEvent->text().toAscii());
